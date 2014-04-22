11efa9d16b4ba09e196ecee9f40e1523a34799b9
coch9894 <coch9894@vandals.uidaho.edu>
2014-04-22 10:05:08 -0700

    Loading a Population
    Untested code for writing and loading a population is done.
    I haven't tested yet because I want a code review for it first.
    The files might cause a problem but we can talk about that later.

--- EvolvedFighting/Pop.cpp
+++ EvolvedFighting/Pop.cpp
@@ -20,11 +20,104 @@ void Pop::load_pop(void)
 	for( int count = 0; count < POP_SIZE; count++ )
 	{
 		delete population[count];
+		population[count] = NULL;
 	}
 	
 	for( int count = 0; count < POP_SIZE; count++ )
 	{
-		std::string str;
+		char* str;
+		std::queue<node*> q;
+
+		str = new char[10];
+		i.getline(str,10);
+
+		if( str[0] == '\n' && count < POP_SIZE )
+		{
+			char temp;
+			std::cout << "Error in loading population" << std::endl;
+			std::cin >> temp;
+			exit(-1);
+		}
+
+		while( str[0] != '8' )
+		{
+			node * temp;
+			temp = new node();
+
+			switch( str[0] )
+			{
+			case turn_left:
+				temp->type = turn_left;
+				break;
+			case turn_right:
+				temp->type =  turn_right;
+				break;
+			case move:
+				temp->type = move;
+				break;
+			case aim:
+				temp->type = aim;
+				break;
+			case shoot:
+				temp->type = shoot;
+				break;
+			case prog3:
+				temp->type = prog3;
+				break;
+			case prog2:
+				temp->type = prog2;
+				break;
+			case iflookingat:
+				temp->type = iflookingat;
+				break;
+			}
+
+			q.push(temp);
+
+			i.getline(str,10);
+		}
+
+		population[count] = q.front();
+		population[count]->parent = NULL;
+		q.pop();
+
+		node * temp = population[count];
+		std::queue<node*> fill;
+		fill.push(temp);
+
+		while( !fill.empty() )
+		{
+			temp = fill.front();
+			fill.pop();
+
+			temp->left = q.front();
+			temp->left->parent = temp;
+			q.pop();
+			if( temp->left != NULL )
+			{
+				fill.push(temp->left);
+			}
+
+			if( temp->type == prog3 )
+			{
+				temp->mid = q.front();
+				temp->mid->parent = temp;
+				q.pop();
+				if( temp->mid != NULL )
+				{
+					fill.push(temp->mid);
+				}
+			}
+
+			temp->right = q.front();
+			temp->right->parent = temp;
+			q.pop();
+			if( temp->right != NULL )
+			{
+				fill.push(temp->right);
+			}
+		}
+
 	}
 }
 
